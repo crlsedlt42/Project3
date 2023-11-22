1a282820ba3998611a4e75e1e9a8a3e5df067fc5
crlse42 <carlosdlt42@hotmail.com>
2023-11-22 13:44:05 -0700

    Added node packages to get links to work and code to the header.jsx and main.jsx

--- client/src/pages/Header.jsx
+++ client/src/pages/Header.jsx
@@ -1,5 +1,6 @@
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 import fabic from '../assets/FabicCodeLogoWhite500px.png';
 
 
@@ -8,15 +9,21 @@ function Header() {
     <div id='whole-header'>
       <div class="flex-for-header">
         <img src={fabic} alt="Logo" id='logo-pic' />
-        <a href='#'>Shop</a>
+        <a href='#'>
+          < Link to='/jewelry'> Shop </Link>
+        </a>
         <a href='#'>About</a>
       </div>
 
       <h1>FabicCode</h1>
 
       <div class="flex-for-header">
-        <a href='#'>Login</a>
-        <a href='#'>Cart(0)</a>
+        <a href='#'>
+        < Link to='/login'> Log-In/Sign-Up </Link>
+        </a>
+        <a href='#'>
+        <Link to ='/cart'> Cart </Link>
+        </a>
       </div>
     </div>
   )
