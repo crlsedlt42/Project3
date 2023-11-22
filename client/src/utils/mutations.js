1a9dced023fba8f8890aa726c6bbb42d0c2c3be8
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2023-11-21 20:45:05 -0700

    mutations for client side

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,41 @@
+import  { gql } from '@apollo/client';
+
+export const login = gql`
+mutation Mutation($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }`;
+
+  export const ADD_ORDER = gql`
+  mutation Mutation($products: [ID]!) {
+    addOrder(products: $products) {
+      purchaseDate
+      products {
+        _id
+        name
+        image
+        description
+        price
+        quantity
+        category {
+          name
+        }
+      }
+    }
+  }
+  `;
+
+  export const ADD_USER = gql`
+  mutation Mutation($firstName: String!, $lastName: String!, $email: String!, $password: String!) {
+    addUser(firstName: $firstName, lastName: $lastName, email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+  `;
