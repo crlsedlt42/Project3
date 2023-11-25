22cce8c606d01e9e00b5168a717a10deeed62b5a
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2023-11-24 17:31:57 -0700

    made multi page throught app.jsx and made global state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,73 @@
-import { useState } from 'react'
-import './App.css'
-import './index.css'
+// import { useState } from 'react'
+// import './App.css'
+// import './index.css'
 
-import LoginForm from './pages/LogIn';
-import Header from './components/Header';
-import HomePage from './pages/HomePage';
-import Jewelry from './pages/Jewelry';
-import Hoodies from './pages/Hoodies';
-import WalkInPage from './pages/Hoodie_WalkIn';
+// import LoginForm from './pages/LogIn';
+// import Header from './components/Header';
+// import HomePage from './pages/HomePage';
+// import Jewelry from './pages/Jewelry';
+// import Hoodies from './pages/Hoodies';
+// import WalkInPage from './pages/Hoodie_WalkIn';
 
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header />
+//       <HomePage />
+//       <Header />
+//       <Jewelry />
+//       <Header />
+//       <Hoodies />
+//       <Header />
+//       <WalkInPage />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import { Outlet } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+import NavBar from './components/Nav';
+import { StoreProvider } from './utils/GlobalState';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <HomePage />
-      <Header />
-      <Jewelry />
-      <Header />
-      <Hoodies />
-      <Header />
-      <WalkInPage />
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <StoreProvider>
+          <NavBar />
+          <Outlet />
+        </StoreProvider>
+      </div>
+    </ApolloProvider>
   );
 }
 
