e7c1285f54ad07efa9caa1dbc4992d0647c9458a
Jdog2001 <134169267+JoshHill1@users.noreply.github.com>
2023-11-30 17:41:42 -0800

    updated about page

--- client/src/pages/about.jsx
+++ client/src/pages/about.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const AboutPage = () => {
-return (
-<div style={{ textAlign: 'center', margin: '30px' }}>
-{/* <img src={logo} alt="FabicCode Logo" style={{ width: '150px', height: '150px', marginBottom: '20px' }} /> */}
-<h1 style={{ color: 'black' }}>About Us</h1>
-<p>FabicCode is a brand that opens up the opportunity for everyone to easily get their hands on new styles with just a few clicks</p>
-<p>Feel free to hang out for a while and explore what we have to offer!</p>
-</div>
-);
+  return (
+    <div style={{ textAlign: "center", margin: "30px" }}>
+      {/* <img src={logo} alt="FabicCode Logo" style={{ width: '150px', height: '150px', marginBottom: '20px' }} /> */}
+      <h1>About Us</h1>
+      <p>
+        Attribute:Attire is a brand that opens up the opportunity for everyone
+        to easily get their hands on new styles with just a few clicks
+      </p>
+      <p>
+        This is not an official site that sells any of the products. This was
+        made for learning purposes only and will not send anything to you if you
+        make an order.
+      </p>
+      {/* add a section of where to contact the devs */}
+    </div>
+  );
 };
 export default AboutPage;
